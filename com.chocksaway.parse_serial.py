4bd33efc87c4809fdef81247f6e753a3131f0ff9
chocksaway <milesd@chocksaway.com>
2014-11-27 20:40:21 +0000

    Bug with closing the drawer

--- com.chocksaway.parse_serial.py
+++ com.chocksaway.parse_serial.py
@@ -1,7 +1,38 @@
 __author__ = 'milesd'
 
 import serial
+import time
+
 
 ser = serial.Serial('/dev/tty.usbmodem1412', 9600)
+
+ts_open = None
+
+def on_open():
+    global ts_open
+    print ("on_open")
+    ts_open = time.time()
+    print ts_open
+
+
+def on_close():
+    print ("on_close")
+    #ts_close  = time.time() - ts_open
+    #print ts_close
+
+
+drawer_open = False
+
 while True:
-    print ser.readline()
+    ####print ("hello world")
+
+    if ser.readline() :
+        print("we have input")
+        my_val = ser.readline()
+        print (my_val)
+        if "Button pushed" in my_val:
+            drawer_open = True
+            on_open()
+        elif "Button NOT pushed" in my_val and drawer_open:
+            drawer_open = False
+            on_close()
