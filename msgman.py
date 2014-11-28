e327687f10eb8937c88378513c0ca410a8946e45
chocksaway <milesd@chocksaway.com>
2014-11-28 14:18:15 +0000

    Mods

--- msgman.py
+++ msgman.py
@@ -34,19 +34,27 @@ def post_a_tweet (user, twtxt):
     miles = 123
 
 
-def gen_tweet_text ():
+def gen_tweet_text():
     """
     Just some random text for tweets.
     """
     ## These are the messages from Mark and Ian
+
     msgs = ["Customer active %s %s",
             "Uh-oh! Sugar rush alert! %s %s",
             "Is it 3pm yet??! %s %s",
             "I feel lighter! %s %s",
             "Chocolate drops! %s %s",
             "Drawer down alert! %s %s",
-            "Help! Help! Someones got their hands in my drawers! %s %s",
+            "Help! Help! Someone’s got their Chocolate Fingers in my draws! %s %s",
             "Chocolate related innuendo? Let me think %s %s",
+            "Don’t make me get my Snickers in a Twix. Replenish, replenish! %s %s",
+            "Hey smarty pants, get your hand out my Smartie draws %s %s",
+            "Ooo, I just felt a Ripple of excitement! %s %s",
+            "Thank Crunchie it’s Friday! %s %s",
+            "Is that Peter? Karney leave the sweets alone? %s %s",
+            "Fill me up, Reece’s Peanut Buttercups! %s %s",
+            "Don’t be greedy, remember to feed me. %s %s",
             "All you need is love. But a little chocolate now and then doesn't hurt %s %s",
             "There is nothing better than a friend, unless it is a friend with chocolate %s %s",
             "Dont wreck a sublime chocolate experience by feeling guilty %s %s",
@@ -54,10 +62,11 @@ def gen_tweet_text ():
             "What you see before you, my friend, is the result of a lifetime of chocolate %s %s",
             "Look, theres no metaphysics on earth like chocolates %s %s",
             '"Draw down"? sounds like 2008! %s %s',
-            "Oh yeah, there's a chocolate for that %s %s",
+            "Oh yeah, there's a sweetie for that %s %s",
             "No, its not 3pm, its 3am. Where have you been?! %s %s",
             "Your drawers or mine? %s %s",
             "Hold on, Im feeling something... %s %s"]
+
 
     return msgs[random.randint(0,len(msgs)-1)]
 
--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -5,28 +5,39 @@ import facebook
 import pytest
 import datetime
 
-def test_zero_division():
-    with pytest.raises(ZeroDivisionError):
-        1 / 0
+
+def messages():
+    HASHTAG_CLOSE = "#drawerofplenty #close"
+
+    msgs = ["Customer active %s %s",
+            "Uh-oh! Sugar rush alert! %s %s",
+            "Is it 3pm yet??! %s %s",
+            "I feel lighter! %s %s",
+            "Chocolate drops! %s %s",
+            "Drawer down alert! %s %s",
+            "Help! Help! Someone’s got their Chocolate Fingers in my draws! %s %s",
+            "Chocolate related innuendo? Let me think %s %s",
+            "Don’t make me get my Snickers in a Twix. Replenish, replenish! %s %s",
+            "Hey smarty pants, get your hand out my Smartie draws %s %s",
+            "Ooo, I just felt a Ripple of excitement! %s %s",
+            "Thank Crunchie it’s Friday! %s %s",
+            "Is that Peter? Karney leave the sweets alone? %s %s",
+            "Fill me up, Reece’s Peanut Buttercups! %s %s",
+            "Don’t be greedy, remember to feed me. %s %s",
+            "All you need is love. But a little chocolate now and then doesn't hurt %s %s",
+            "There is nothing better than a friend, unless it is a friend with chocolate %s %s",
+            "Dont wreck a sublime chocolate experience by feeling guilty %s %s",
+            "Anything is good if its made of chocolate %s %s",
+            "What you see before you, my friend, is the result of a lifetime of chocolate %s %s",
+            "Look, theres no metaphysics on earth like chocolates %s %s",
+            '"Draw down"? sounds like 2008! %s %s',
+            "Oh yeah, there's a sweetie for that %s %s",
+            "No, its not 3pm, its 3am. Where have you been?! %s %s",
+            "Your drawers or mine? %s %s",
+            "Hold on, Im feeling something... %s %s"]
+
+    for each in msgs:
+        print (each % (str(123), HASHTAG_CLOSE))
 
 
-def test_write_csv_file():
-    today = datetime.date.today()
-
-    write_string = "27-11-2014 11:03:12,,The drawer is open, Facebook\n"
-
-    with open('/tmp/testCsvOutput.csv', 'a') as the_file:
-        the_file.write("27-11-2014 11:03:12,,The drawer is open, Facebook\n")
-        the_file.write("27-11-2014 11:03:12,27-11-2014 11:04:12,The drawer is closing, Facebook\n")
-        the_file.write("27-11-2014 11:30:15,,The drawer is open, Facebook\n")
-        the_file.write("27-11-2014 11:30:15,27-11-2014 11:31:00,The drawer is closing, Facebook\n")
-        the_file.write("27-11-2014 11:34:02,,The drawer is open, Facebook\n")
-        the_file.write("27-11-2014 11:34:02,27-11-2014 11:36:13,The drawer has closed, Facebook\n")
-    assert(1 == 1)
-
-
-def test_post_to_facebook():
-    graph = facebook.GraphAPI("CAALZA3vgt0TYBAHghbwefK9lK3qB2kCFSwHZBxRON8wVcUYA19jpyIFLsW9qiSj7QcnlxMllxNck88AGpYjRZBmTSqB3ZAto0TvXHprwEGwgtHJJluqXTbf9Jynph8ZA52k6g3bakCZBYFxzvmnLsWkCXvSRsXnzwIj10P2vmsMoqVq0lPb1sOlfqZCflWLCp6gzYvSZBzD1aGZA51xV25IbL")
-    profile = graph.get_object("me")
-    friends = graph.get_connections("me", "friends")
-    graph.put_object("me", "feed", message="I am writing on my wall!")
+messages()
